c3c0d8418b85993b7d8419093c4dd0b056e23b6b
haniffzain <haniffsp@gmail.com>
2026-08-01 22:34:27 +0800

    feat(renderer): add desktop render controller

---
+++ launcher/RavenLauncher/app/src/main/java/com/raven/launcher/renderer/DesktopRenderController.kt
@@ -0,0 +1,46 @@
+package com.raven.launcher.renderer
+
+import com.raven.launcher.events.LumaEvent
+import com.raven.launcher.events.LumaEventBus
+import com.raven.launcher.events.LumaEventListener
+import com.raven.launcher.events.desktop.DesktopMovedEvent
+
+/**
+ * Keeps the desktop renderer synchronized
+ * with desktop events.
+ */
+class DesktopRenderController(
+
+    private val renderer: DesktopRenderer
+
+) : LumaEventListener {
+
+    init {
+
+        LumaEventBus.register(this)
+
+    }
+
+    override fun onEvent(
+        event: LumaEvent
+    ) {
+
+        when (event) {
+
+            is DesktopMovedEvent -> {
+
+                renderer.render()
+
+            }
+
+        }
+
+    }
+
+    fun destroy() {
+
+        LumaEventBus.unregister(this)
+
+    }
+
+}
